b079028065f5f8637cd7473d9e820b01a4007d6c
TAmyG <tamy.vivas@gmail.com>
2018-12-22 15:48:26 -0600

    feat: Agregado paginas con rango
    Utilizacion de ngChanges para actualizar rangos de paginas

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 import { _appIdRandomProviderFactory } from '@angular/core/src/application_tokens';
 
 @Component({
@@ -6,16 +12,46 @@ import { _appIdRandomProviderFactory } from '@angular/core/src/application_token
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.css']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
   @Input()
   paginador: any;
 
   paginas: number[];
+  desde: number;
+  hasta: number;
+
   constructor() {}
 
   ngOnInit() {
-    this.paginas = new Array(this.paginador.totalPages)
-      .fill(0)
-      .map((_valor, indice) => indice + 1);
+    this.initPaginator();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const paginadoActualizado = changes['paginador'];
+
+    if (paginadoActualizado.previousValue) {
+      this.initPaginator();
+    }
+  }
+
+  private initPaginator(): void {
+    this.desde = Math.min(
+      Math.max(1, this.paginador.number - 4),
+      this.paginador.totalPages - 5
+    );
+    this.hasta = Math.max(
+      Math.min(this.paginador.totalPages, this.paginador.number + 4),
+      6
+    );
+
+    if (this.paginador.totalPages > 5) {
+      this.paginas = new Array(this.hasta - this.desde + 1)
+        .fill(0)
+        .map((_valor, indice) => indice + this.desde);
+    } else {
+      this.paginas = new Array(this.paginador.totalPages)
+        .fill(0)
+        .map((_valor, indice) => indice + 1);
+    }
   }
 }
